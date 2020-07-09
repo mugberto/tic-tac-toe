9e7c8e4fe260860dc1bd6184c73218b17e83fcd2
Hubert MUGABO <mugberto@gmail.com>
2020-07-10 01:27:54 +0200

    Add first player's play turn interface

--- bin/main.rb
+++ bin/main.rb
@@ -37,3 +37,24 @@ puts "Enter the second player's name (default: Player B)"
 player_two_name = gets.chomp
 player_two_symbol = 'O'
 system('clear') # Refresh the terminal
+
+puts "\n"
+puts 'TIC TAC TOE'
+puts '-----------'
+puts "\n"
+
+puts "#{player_one_name}(#{player_one_symbol})  VS  #{player_two_name}(#{player_two_symbol})\n"
+
+k = 1
+3.times do
+  puts '-------------'
+  3.times { |i| print "| #{k + i} " }
+  puts '|'
+  k += 3
+  puts '-------------' if k > 7
+end
+
+puts "It's your turn #{player_one_name}, choose a cell number"
+gets.chomp
+
+system('clear')
