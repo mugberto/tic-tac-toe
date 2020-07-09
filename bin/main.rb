74abd5b2cf8a1ff882f62845df527ea2644e8419
Hubert MUGABO <mugberto@gmail.com>
2020-07-10 01:17:55 +0200

    Add introduction to the game

--- bin/main.rb
+++ bin/main.rb
@@ -1,3 +1,25 @@
 #!/usr/bin/env ruby
 
-puts 'Hello World!'
+puts "\n"
+puts 'TIC TAC TOE'
+puts '-----------'
+puts "\n"
+
+k = 1
+3.times do
+  puts '-------------'
+  3.times { |i| print "| #{k + i} " }
+  puts '|'
+  k += 3
+  puts '-------------' if k > 7
+end
+
+puts "\n"
+puts 'In this game, two players play in turns.
+Each player chooses a number in the checkboard
+The winner is the one who aligns three of his symbols
+either horizontally, vertically or on the diagonal\n'
+
+puts "Press 'Enter' to continue"
+gets
+system('clear')
