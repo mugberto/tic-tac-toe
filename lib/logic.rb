96eaf78cafea80105bcd118ff6107820dd858461
Hubert MUGABO <mugberto@gmail.com>
2020-07-10 17:44:09 +0200

    Modify display method to take arguments
    The arguments are a number and a symbol
    The symbol is placed in a cell corresponding
    to the number.

--- lib/logic.rb
+++ lib/logic.rb
@@ -1,13 +1,24 @@
 class Board
-  def display
-    k = 0
+  def initialize
+    @cell = {}
+  end
+
+  def display(*args)
+    @cell[args[0]] = args[1]
+    @k = 0
     (0..2).each do |i|
       puts '-' * 13 if i.zero?
-      (0..2).each { |j| print j.zero? ? "| #{k += 1} |" : " #{k += 1} |" }
+      (0..2).each do |j|
+        if @cell.key?(@k + 1)
+          print j.zero? ? "| #{@cell[@k += 1]} |" : " #{@cell[@k += 1]} |"
+        else
+          print j.zero? ? "| #{@k += 1} |" : " #{@k += 1} |"
+        end
+      end
       puts "\n" + '-' * 13
     end
   end
 end
 
 board = Board.new
-board.display
+board.display(2, 'O')
